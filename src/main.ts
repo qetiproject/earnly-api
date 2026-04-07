a6b0c917273f717f07dd3f14605fe8650a299ab3
qetiproject <ketideveloper@gmail.com>
2026-04-07 15:43:39 +0400

    remove global interceptor

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  // app.useGlobalInterceptors(
+  //   new ClassSerializerInterceptor(app.get(Reflector)),
+  // );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
